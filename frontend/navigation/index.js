5f603c29e9e68a5cd2999ca96867d0cdfc96a109
THE HUNG <52951651+JamesHust@users.noreply.github.com>
2021-04-17 10:14:10 +0700

    remove old frontend

--- frontend/navigation/index.js
+++
@@ -1,9 +0,0 @@
-import React from "react";
-import Routes from "./Routes";
-
-//Component khởi tạo chung cho cả thư mục navigation
-const Providers = () => {
-  return <Routes />;
-};
-
-export default Providers;
--- frontend/utils/Common.js
+++
@@ -1,3 +0,0 @@
-export const addRequireSourceImg = (path) => {
-    return `require("${path}")`;
-}
